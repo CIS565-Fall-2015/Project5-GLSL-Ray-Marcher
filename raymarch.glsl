ea572f2e8ac7d2fa695030ac9b3c787160829fdf
ZIWEI <GITHUB_USER@users.noreply.github.com>
2015-10-15 22:56:52 -0400

    transform

--- raymarch.glsl
+++ raymarch.glsl
@@ -4,15 +4,63 @@
 
 int itrNum = 0;
 
+vec3 TransP(vec3 pos, vec3 T, vec3 R, vec3 S)
+{
+    float c1 = cos(R.x);
+    float c2 = cos(R.y);
+    float c3 = cos(R.z);
+    float s1 = sin(R.x);
+    float s2 = sin(R.y);
+    float s3 = sin(R.z);
+    mat4 RotMat = mat4(	c2*c3,	c1*s3+c3*s1*s2,	s1*s3-c1*c3*s2,	0,
+                      	-c2*s3,	c1*c3-s1*s2*s3,	c3*s1+c1*s2*s3,	0,
+                      	s2,		-c2*s1,			c1*c2,			0,
+                      	0,		0,				0,				1);
+    mat4 inv_R = mat4(	c2*c3,	-c2*s3,	s2,	0,
+                     	c1*s3+c3*s1*s2,	c1*c3-s1*s2*s3,-c2*s1,0,
+                     	s1*s3-c1*c3*s2,c3*s1+c1*s2*s3,c1*c2,0,	
+                     	0,0,0,1);
+    mat4 inv_T = mat4(1,0,0,0,
+                      0,1,0,0,
+                      0,0,1,0,
+                      -T.x,-T.y,-T.z,1);
+    mat4 inv_S = mat4(1.0/S.x,0,0,0,
+                      0,1.0/S.y,0,0,
+                      0,0,1.0/S.z,0,
+                      0,0,0,1);
+    
+    mat4 inv_M = inv_S*inv_R*inv_T;
+    
+    vec4 localPos = inv_M*vec4(pos,1.0);
+    
+    return localPos.xyz;
+}
+
 float sdSphere(vec3 p, float s)
 {
 	return length(p) - s;
 }
 
+float sdPlane(vec3 p)
+{
+	return p.y;
+}
+
+vec4 opU(vec4 d1, vec4 d2)
+{
+	return (d1.w<d2.w) ? d1 : d2;
+}
+
 
 vec4 map(in vec3 pos)
 {
-	return vec4(vec3(50, 0, 0), sdSphere(pos, 0.5));
+    vec3 Ts = vec3(0,1.0,0.0);
+    vec3 Rs = vec3(0,0.0,0.0);
+    vec3 Ss = vec3(1.0,2.0,1.0);
+    vec4 sphere = vec4(vec3(0.8, 0, 0), sdSphere(TransP(pos,Ts,Rs,Ss), 0.5));
+    vec4 plane = vec4(vec3(1, 1, 0.5), sdPlane(pos));
+    vec4 res = opU(sphere,plane);
+	return res;
 }
 
 vec3 calcNormal( in vec3 pos )
@@ -38,7 +86,7 @@ vec4 castRay_Naive(in vec3 ro, in vec3 rd)
 		vec4 res = map(ro + rd*t);
 		if (res.w<precis)
 		{
-			m = calcNormal(ro + rd*t);//res.xyz;
+			//m = calcNormal(ro + rd*t);//res.xyz;
             itrNum = i;
 			break;
 		}
@@ -74,7 +122,7 @@ vec4 castRay_ST(in vec3 ro, in vec3 rd)
         }
 		t += res.w;
 		m = res.xyz;
-        m = calcNormal(ro + rd*t);
+        //m = calcNormal(ro + rd*t);
 	}
 
 	if (t>tmax) t = -1.0;
@@ -83,16 +131,30 @@ vec4 castRay_ST(in vec3 ro, in vec3 rd)
 
 vec3 render(in vec3 ro, in vec3 rd) {
 	// TODO
-	vec3 col = vec3(0.8, 0.9, 1.0);
+
 	vec4 res = castRay_ST(ro, rd);
 	float t = res.w;
+    vec3 col = vec3(0.8, 0.9, 1.0);
+    vec3 nor = calcNormal(ro + rd*t);
 	vec3 m = res.xyz;
 	if (t>-0.5)  // Ray intersects a surface
 	{
 		// material        
 		//col = m;
-        col = 0.45 + 0.3*sin(vec3(0.05, 0.08, 0.10)*(float(itrNum) - 6.0));
+        //col = mix( col, vec3(0.8,0.9,1.0), 1.0-exp( -0.0005*t*t ) );
+        col = nor;
+        //col = 0.45 + 0.3*sin(vec3(0.05, 0.08, 0.10)*(float(itrNum) - 6.0));
 	}
+    
+    vec3  lig = normalize( vec3(-0.6, 0.7, -0.5) );
+    float amb = clamp( 0.5+0.5*nor.y, 0.0, 1.0 );
+    float dif = clamp( dot( nor, lig ), 0.0, 1.0 );
+    
+    vec3 brdf = vec3(0.0);
+    brdf += 1.20*dif*vec3(1.0,1,1);
+
+   // col = col*brdf;
+
 	return vec3(clamp(col, 0.0, 1.0));
 	//return rd;  // camera ray direction debug view
 }
