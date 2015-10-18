4385340a162f4f544e2315772a27c1e0b4113b39
Kangning Li <likangning93@gmail.com>
2015-10-17 20:31:26 -0400

    producing a crappy sphere.

--- raymarch.glsl
+++ raymarch.glsl
@@ -10,7 +10,7 @@ mat3 eulerXYZRotationMatrix(in vec3 rotation) {
     float s3 = sin(rotation.z);
 
     eulerXYZ[0][0] = c2 * c3;
-    eulerXYZ[0][1] = s1 * s3 + c1 * c3 * c2;
+    eulerXYZ[0][1] = s1 * s3 + c1 * c3 * s2;
     eulerXYZ[0][2] = c3 * s1 * s2 - c1 * s3;
 
     eulerXYZ[1][0] = -s2;
@@ -35,7 +35,7 @@ mat3 eulerZYXRotationMatrix(in vec3 rotation) {
     float s3 = sin(rotation.z);
     
     eulerZYX[0][0] = c1 * c2;
-    eulerZYX[0][1] = c2 * s2;
+    eulerZYX[0][1] = c2 * s1;
     eulerZYX[0][2] = -s2;
 
     eulerZYX[1][0] = c1 * s2 * s3 - c3 * s1;
@@ -46,7 +46,7 @@ mat3 eulerZYXRotationMatrix(in vec3 rotation) {
     eulerZYX[2][1] = c3 * s1 * s2 - c1 * s3;
     eulerZYX[2][2] = c2 * c3;
     
-    return eulerXYZ;
+    return eulerZYX;
 }
 
 
@@ -54,18 +54,25 @@ mat3 eulerZYXRotationMatrix(in vec3 rotation) {
 // each takes in transformations and outputs the distance from the point to the
 // ray in world space.
 
-
+// unit sphere has radius of 1
 float sphere(in vec3 point, in vec3 translation, in vec3 scale, in vec3 rotation) {
     // transform the point into local coordinates
     vec3 localPoint = point;
     localPoint -= translation; // untranslate
-                               // unrotate
-    localPoint /= scale;       // unscale
+    localPoint = eulerZYXRotationMatrix(-1.0 * rotation) * localPoint; // unrotate
+    localPoint.x /= scale.x; // unscale
+    localPoint.y /= scale.y;
+    localPoint.z /= scale.z;
 
     // compute distance from the unit sphere
+    vec3 localDist = localPoint - normalize(localPoint);
 
-    // transform into world distance
-    // the distance is along the vector point - center, so use that in the scaleback.
+    // transform into world space
+    // the distance is along the vector point - center, so use that in the scaleback?
+    localDist.x *= scale.x;
+    localDist.y *= scale.y;
+    localDist.z *= scale.z;
+    return length(localDist);
 }
 
 
@@ -74,28 +81,48 @@ float sphere(in vec3 point, in vec3 translation, in vec3 scale, in vec3 rotation
 
 // returns the conservative distance to the nearest scene object.
 // declare all scene objects in here.
-float sceneGraphCheck(in vec3 pos)
+float sceneGraphCheck(in vec3 point)
 {
-
+    float sphere1 = sphere(point, vec3(0.0, 0.0, 0.0), vec3(1.0, 1.0, 1.0), vec3(0.0, 0.0, 0.0));
+    return sphere1;
 }
 
 // returns a t along the ray that hits the first intersection.
 // uses naive stepping [McGuire 4]
-float castRayNaive(in vec3 rayPosition, in vec3 rayDirection)
+vec2 castRayNaive(in vec3 rayPosition, in vec3 rayDirection)
 {
+    float tmin = 1.0;
+    float stepSize = 0.01; // 2000 * 0.01 gives us a max distance of 20
+    float epsilon = 0.002;
 
+    float t = tmin;
+    float distance = 1000.0;
+    float maxedOut = 1.0; // toggle for whether the ray maxed out or not
+    vec3 point = rayPosition;
+    for (int i = 0; i < 2000; i++) {
+        point = rayPosition + rayDirection * t;
+        distance = sceneGraphCheck(point);
+        t += stepSize;
+        if (distance < epsilon) {
+            maxedOut = -1.0;
+        }
+    }
+    return vec2(t, maxedOut);
 }
 
 // returns a t along the ray that hits the first intersection
 // uses spherical stepping [McGuire 6]
 float castRaySphere(in vec3 rayPosition, in vec3 rayDirection)
 {
-
+    return -1.0;
 }
 
 vec3 render(in vec3 ro, in vec3 rd) {
-    // TODO
-    return rd;  // camera ray direction debug view
+    vec2 distanceMaterial = castRayNaive(ro, rd);
+    if (distanceMaterial[1] < 0.0) {
+        return vec3(1.0, 1.0, 1.0);
+    }
+    return vec3(0.0, 0.0, 0.0);  // camera ray direction debug view
 }
 
 mat3 setCamera(in vec3 ro, in vec3 ta, float cr) {
