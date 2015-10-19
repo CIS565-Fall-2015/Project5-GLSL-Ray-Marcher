93a3edda5f21ac8650c14133f853e19d4f6ffa06
Terry Sun <terrynsun@gmail.com>
2015-10-19 17:36:53 -0400

    Height mapping.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# [CIS565 2015F] Shadertoy
+# [CIS565 2015F] Shadertoy: Morph
 
 **GLSL Ray Marching**
 
@@ -6,7 +6,7 @@
 
 Terry Sun; Google Chrome 45.0, Arch Linux, Intel i5-4670, GTX 750
 
-### Live on Shadertoy (TODO)
+### Live on Shadertoy
 
 [![](img/thumb.png)](https://www.shadertoy.com/view/XISSRc)
 
@@ -20,6 +20,8 @@ This Shadertoy uses *code* from the following resources:
 * Iñigo Quílez's [Modeling with distance functions]
   (http://www.iquilezles.org/www/articles/distfunctions/distfunctions.htm) --
   [Shadertoy] (https://www.shadertoy.com/view/Xds3zN)
+* Iñigo Quílez's [Terrain Raymarching]
+  (http://www.iquilezles.org/www/articles/terrainmarching/terrainmarching.htm)
 * "Enhanced Sphere Tracing." Keinert, Schafer, Korndorf, Ganse, Stamminger.
   [PDF](http://erleuchtet.org/~cupe/permanent/enhanced_sphere_tracing.pdf)
 
@@ -33,13 +35,19 @@ Rererence: iq "Raymarching Primitives", McQuire "Implicitly Defined Surfaces"
 
 #### Soft Shadows
 
-Rererence: iq "Modeling with distance functions"
+Reference: iq "Modeling with distance functions"
 
 ![](img/debug_softshadow_noise.png)
 
+#### Height Mapped Terrain
+
+Reference: iq "Terrain Raymarching"
+
+![](img/height.png)
+
 #### Sphere Overrelaxation
 
-Reference: McQuire "Implicitly Defined Surfaces", "Enhanced Sphere Tracing"
+Reference: McQuire "Implicitly Defined Surfaces", Keinert "Enhanced Sphere Tracing"
 
 The number of ray march iterations displayed as grayscale, with darker areas
 indicating fewer iterations before the surface is considered intersected.
@@ -64,6 +72,6 @@ Overrelaxation without stepping backwards:
 
 ![](img/bloop_overrelax_gloopy.png)
 
-Accidentally broke naive raymarching and got wireframes (position colors):
+Naive ray marching with a step size of 0.1 (too small), got wireframes:
 
 ![](img/bloop_wireframe.png)
--- raymarch.glsl
+++ raymarch.glsl
@@ -3,14 +3,15 @@ const vec3 light = vec3(2.0, 5.0, -1.0);
 const vec3 color = vec3(0.85, 0.85, 0.85);
 const vec3 ambient = vec3(0.05, 0.05, 0.05);
 const vec3 background = vec3(0.2, 0.5, 0.5);
+const vec3 ground = vec3(0.2, 0.8, 0.8);
 
-const vec3 scene2cam = vec3(.25, 2.25, -2.2);
+const vec3 scene2cam = vec3(.35, 2.5, -2);
 //const vec3 scene2cam = vec3(2.5, 2.2, -.8);
 const float EPSILON = 0.01;
 const float TMIN = 0.01;
 
 // Debug flags
-#define OVERRELAX   1
+#define OVERRELAX   0
 #define SPHERETRACE 1
 #define NAIVE       0
 
@@ -18,9 +19,12 @@ const float TMIN = 0.01;
 #define NORMAL   0
 #define ITERS    0
 
-#define SHADOW   1
+#define SHADOW   0
 
 #define FIXEDCAM 1
+
+#define SCENE1 0
+#define SCENE2 1
 
 /*************************** Signed distance functions ***********************
  * McGuire: http://graphics.cs.williams.edu/courses/cs371/f14/reading/implicit.pdf
@@ -105,23 +109,20 @@ vec3 tr(vec3 p, vec3 translate) {
  * McGuire: http://graphics.cs.williams.edu/courses/cs371/f14/reading/implicit.pdf
  */
 
-#define SCENE1 0
-#define SCENE2 1
 float nearestIntersection(in vec3 p) {
 #if SCENE1
     float t = sdSphere(p, 0.5);
-    t = setunion(t, sdPlane     (tr(p, vec3(-2.0)), vec3(.0, 1.0, .0))             );
+    //t = setunion(t, sdPlane     (tr(p, vec3(-2.0)), vec3(.0, 1.0, .0))             );
     t = setunion(t, sdTorus     (tr(p, vec3(-1.3,.8,-1.3)), .4, .15)               );
     t = setunion(t, sdTorus88   (tr(p, vec3(.0,.0,-1.5)), vec2(.35,.2))            );
     t = setunion(t, sdCapsule   (tr(p, vec3(.6, .0, .6)), vec3(1.0), vec3(.5), .2) );
     t = setunion(t, sdRoundedBox(tr(p, vec3(-1.0, 1.0, .0)), vec3(.2), .1)         );
-    t = setunion(t, sdPlane     (tr(p, vec3(-2.0)), vec3(.0, 1.0, .0))             );
     t = setunion(t, sdCylinder  (tr(p, vec3(1.2,.0,.0)), .5, .2)                   );
     t = setunion(t, sdTriPrism  (tr(p, vec3(.0,.0,1.5)), vec2(.4, .4))             );
 #endif
 #if SCENE2
-    float t = sdCylinder(tr(p, vec3(-.1, 0, 0)), 1.5, .5);
-    t = setunion(t, sdPlane     (tr(p, vec3(-2.0)), vec3(.0, 1.0, .0))   );
+    float t = sdCylinder(tr(p, vec3(-.1, .25, 0)), 1.5, .25);
+    //t = setunion(t, sdPlane     (tr(p, vec3(-2.0)), vec3(.0, 1.0, .0))   );
     t = subtract(t, sdTriPrism  (tr(p, vec3(-.1,1,0)), vec2(1., 1.))     );
 
     t = setunion(t, sdSphere    (tr(p, vec3(1,1,0)), .2)                 );
@@ -136,7 +137,7 @@ float nearestIntersection(in vec3 p) {
 // Naive iteration
 vec2 raymarch_naive(in vec3 ro, in vec3 rd) {
     const float tmax = 30.0;
-    const float tstep = 0.1;
+    const float tstep = 0.01;
     int iters = 0;
     for (float t = 0.1; t < tmax; t += tstep) {
         iters++;
@@ -173,7 +174,7 @@ vec2 raymarch_sphere(in vec3 ro, in vec3 rd) {
 vec2 raymarch_overrelax(in vec3 ro, in vec3 rd) {
     int iters = 0;
     const float tmax = 30.0;
-    float t = 0.0;
+    float t = 0.1;
     float dt = 0.0;
     float extrastep = 0.0;
     float K = 1.0;
@@ -190,7 +191,7 @@ vec2 raymarch_overrelax(in vec3 ro, in vec3 rd) {
             // Take K * intersection distance step forward
             // Unless that is too large; then step backwards
             float nstep = K * intersection;
-            if (intersection < extrastep) {
+            if (intersection < dt) {
                 t -= (dt + extrastep);
                 break;
             } else {
@@ -206,7 +207,7 @@ vec2 raymarch_overrelax(in vec3 ro, in vec3 rd) {
     }
 
     for (int i = 0; i < 50; i++) {
-        iters++;
+        //iters++;
         vec3 p = ro + rd * t;
         float intersection = nearestIntersection(p);
         if (intersection > 0.0 && intersection < TMIN) {
@@ -223,13 +224,13 @@ vec2 raymarch_overrelax(in vec3 ro, in vec3 rd) {
 
 vec2 raymarch(in vec3 ro, in vec3 rd) {
     #if OVERRELAX
-    return raymarch_overrelax(ro, rd);
+   	    return raymarch_overrelax(ro, rd);
     #endif
     #if SPHERETRACE
-    return raymarch_sphere(ro, rd);
+        return raymarch_sphere(ro, rd);
     #endif
     #if NAIVE
-    return raymarch_naive(ro, rd);
+        return raymarch_naive(ro, rd);
     #endif
 }
 
@@ -280,45 +281,99 @@ vec3 shadow(in vec3 p, in vec3 color) {
     return clamp(shadow * color, 0.0, 1.0) + ambient;
 }
 
+// http://www.iquilezles.org/www/articles/terrainmarching/terrainmarching.htm
+float heightf(vec2 xz) {
+    return -2.0 - (sin(xz.y*2.0)/5.0) - (sin(xz.x)/4.0);
+}
+
+float heightff(float x, float z) {
+    return heightf(vec2(x, z));
+}
+
+vec3 heightn(vec3 p) {
+    float eps = 0.001;
+    vec3 n = vec3(heightff(p.x-eps,p.z) - heightff(p.x+eps,p.z),
+                         2.0*eps,
+                         heightff(p.x,p.z-eps) - heightff(p.x,p.z+eps) );
+    return normalize(n);
+}
+
+vec2 heightmarch(in vec3 ro, in vec3 rd) {
+    const float tmax = 30.0;
+    const float tstep = 0.01;
+    int iters = 0;
+    for (float t = 0.1; t < tmax; t += tstep) {
+        iters++;
+        vec3 rayp = ro + rd * t;
+        float height = heightf(rayp.xz);
+        
+        if (rayp.y < height) {
+            return vec2(t, iters);
+        }
+    }
+    return vec2(-1);
+}
+
 vec3 render(in vec3 ro, in vec3 rd) {
     vec2 ray = raymarch(ro, rd);
     float d = ray.x;
-    float iters = ray.y;
-    if (d > 0.0) {
-        #if ITERS
-            return vec3(iters/20.);
-        #endif
-
-        vec3 p = ro + rd*d;
-        #if DISTANCE
-            return p;
-        #endif
-
-        vec3 n = normalAt(p);
-
-        #if NORMAL
-            return n;
-        #endif
-        
-        vec3 basecolor = color;
-        if (p.y > -1.9 && p.y < .6) {
-            basecolor = color;
-        } else if (p.y > -1.9) {
-            vec3 absp = abs(p);
-            basecolor = absp;
+    vec3 p = ro + rd*d;
+    
+    vec3 basecolor = color;    
+    
+    if (d < 0.0) {
+        ray = heightmarch(ro, rd);
+        d = ray.x;
+        if (ray.y < 0.0) {
+            return background;
         }
-
-        vec3 c = lambert(p, n, basecolor);
-
+        
+        vec3 rayp = ro + rd * d;
+        float y = heightf(p.xz);
+        p = vec3(rayp.x, y, rayp.z);
+        basecolor = background;
+        vec3 n = heightn(p);
+        vec3 c = lambert(p, n, ground);
         #if SHADOW
-            c = shadow(p - 3.0*rd*EPSILON, c);
+            c = shadow(p, c);
             c = clamp(c, 0.0, 1.0);
         #endif
-
         return c;
-    } else {
-        return background;
     }
+    
+    float iters = ray.y;
+    
+    #if ITERS
+    return vec3(iters/20.);
+        #endif
+
+    #if DISTANCE
+        return p;
+    #endif
+
+    vec3 n = normalAt(p);
+
+    #if NORMAL
+        return n;
+    #endif
+
+    #if SCENE2
+    if (p.y > -1.9 && p.y < .6) {
+        basecolor = color;
+    } else if (p.y > -1.9) {
+        vec3 absp = abs(p);
+        basecolor = absp;
+    }
+    #endif
+
+    vec3 c = lambert(p, n, basecolor);
+
+    #if SHADOW
+        c = shadow(p - 3.0*rd*EPSILON, c);
+        c = clamp(c, 0.0, 1.0);
+    #endif
+
+    return c;
 }
 
 /************************************ Setup ***********************************
