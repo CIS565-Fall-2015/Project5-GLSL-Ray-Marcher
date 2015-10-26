6f26c6359e7191b7ab6aed6600365ed6fa3d5c0b
Dome P <me@rpdome.com>
2015-10-26 14:44:13 -0400

    img

--- raymarch.glsl
+++ raymarch.glsl
@@ -1,7 +1,102 @@
-vec3 render(in vec3 ro, in vec3 rd) {
-    // TODO
-    return rd;  // camera ray direction debug view
+#define maxObj	10
+#define step 	0.1
+#define maxDist	1e10
+#define maxIter 50
+#define epsilon	0.001
+
+#define FIXED_STEP
+
+struct Material{
+    vec3 clr;
+};
+
+struct Object{
+    Material m;
+    float sd;
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+float sphereDist(vec3 p, vec3 c, float r){
+	return length(p-c) - r;
 }
+
+float planeDist(vec3 p, vec3 c, vec3 n){
+	return dot((p-c),n);
+}
+
+Object opUnion(Object a, Object b){
+    if(a.sd < b.sd)
+        return a;
+  	else
+        return b;
+}
+
+Object intersects(in vec3 p){
+
+    Material rDiffuse;
+    rDiffuse.clr = vec3(1.0, 0.0, 0.0);
+
+    Object plane1;
+    plane1.m = rDiffuse;
+    plane1.sd = planeDist(p, vec3(0.0), vec3(0.0, 1.0, 0.0));
+
+
+    Object obj;
+    obj.sd = -1.0;
+
+    obj = opUnion(obj, plane1);
+
+    return obj;
+}
+
+//from inigo quilez's code
+vec3 calcNormal(in vec3 pos)
+{
+	vec3 eps = vec3( 0.001, 0.0, 0.0 );
+	vec3 nor = vec3(
+	    intersects(pos+eps.xyy).sd - intersects(pos-eps.xyy).sd,
+	    intersects(pos+eps.yxy).sd - intersects(pos-eps.yxy).sd,
+	    intersects(pos+eps.yyx).sd - intersects(pos-eps.yyx).sd );
+	return normalize(nor);
+}
+
+
+vec3 getLambertClr(in vec3 p, in Material m){
+    vec3 lpos = vec3(2.0, 2.0, 2.0);
+    vec3 lcol = vec3(1.0, 1.0, 1.0);
+
+    vec3 n = calcNormal(p);
+    vec3 ldir = lpos - p;
+    return clamp(dot(ldir, n), 0.0, 1.0) * m.clr * lcol;
+}
+
+
+float getStep(){
+    return step;
+}
+
+vec3 render(in vec3 ro, in vec3 rd) {
+   	float t = 0.0;
+    vec3 p;
+    Object obj;
+
+    for(int i = 0; i < maxIter; i++)
+    {
+        t += getStep();
+        if(t > maxDist) break;
+
+        p = ro + (rd * t);
+        obj = intersects(p);
+        if(obj.sd > 0.0 && obj.sd < epsilon){
+            return getLambertClr(p, obj.m);
+        }
+    }
+
+    return vec3(0,0,0);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 mat3 setCamera(in vec3 ro, in vec3 ta, float cr) {
     // Starter code from iq's Raymarching Primitives
